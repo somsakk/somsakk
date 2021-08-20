c84e34c34406e5461f6c780ef9d38df2e4d5ba50
somsakk <88964214+somsakk@users.noreply.github.com>
2021-08-20 14:39:55 +0700

    Add files via upload

--- lec03_pointer_structure.c
+++ lec03_pointer_structure.c
@@ -11,7 +11,7 @@ typedef struct {
 int main () {
     Point p1;
     Point p2;
-    Point *paddr;
+    Point *paddr, *paddr2;
 
     /* dot notation */
     p1.x = 1; p1.y = 2;
@@ -19,16 +19,11 @@ int main () {
     printf("p1(%d,%d) and p2(%d,%d)\n", p1.x, p1.y, p2.x, p2.y);
     printf("Addreses: &p1=%p, &p2=%p, paddr=%p\n", &p1, &p2, paddr);
     
-    // printf("*p = %d\n", *paddr); // Reading the value of the address pointerd by a null pointer creshes the program 
-    // To avoid this error, we could check first if the pointer is not null before accessing the variable value it points to
-//     if (paddr) {
-//         printf("*p = %d\n", *paddr); // Reading the value of the address pointerd by a null pointer creshes the program 
-//     }
-
     // Observe the addresses of the elements in the two structures.
     printf("Addreses of x,y for p1 and p2: &p1.x=%p, &p1.x=%p, &p2.x=%p, &p2.y=%p\n", &p1.x, &p1.y, &p2.x, &p2.y);
 
     paddr = &p1;
+    paddr2 = &p2;
     printf("Addreses: &p1=%p, &p2=%p, paddr=%p\n", &p1, &p2, paddr);
 
     // arrow notation
@@ -37,6 +32,5 @@ int main () {
     // We can assign the structure
     p1 = p2;
     printf("p1(%d,%d) and p1(%d,%d)\n", p1.x, (&p1)->y, paddr->x, (*paddr).y);
-
 }
 
