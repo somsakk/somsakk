dc845b0d7a6d19c320c3f6a1ae80b6f1d0f1e373
somsakk <88964214+somsakk@users.noreply.github.com>
2023-10-20 12:31:47 +0700

    Add files via upload

--- riscv_assembly/branch_ex0.s
+++ riscv_assembly/branch_ex0.s
@@ -1,10 +1,10 @@
-        addi s0, zero, 4
-        addi s1, zero, 1
-Shift:  slli s1, s1, 2
-        beq s0, s1, target
+        li s0, 4  # pseudo-inst to load imm 4 to reg. s0
+        li s1, 1
+Shift:  slli s1, s1, 2 # label this inst as Shift
+        beq s0, s1, target # conditional branching
         addi s1, s1, 1
         sub s1, s1, s0
-target: add s1, s1, s0
-        j Shift
+target: add s1, s1, s0 # label this inst as target
+        j Shift  # unconditional jump. Pseudo-inst to jump to inst labelled Shift
         nop
 ## branch_ex0.s ##
