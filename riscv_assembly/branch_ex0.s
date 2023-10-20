87452a290bb8257ccbcf05e7b886b0fd813c37ff
somsakk <88964214+somsakk@users.noreply.github.com>
2023-10-20 12:24:26 +0700

    Add files via upload

--- riscv_assembly/branch_ex0.s
+++ riscv_assembly/branch_ex0.s
@@ -1,10 +1,10 @@
         addi s0, zero, 4
         addi s1, zero, 1
-        slli s1, s1, 2
+Shift:  slli s1, s1, 2
         beq s0, s1, target
         addi s1, s1, 1
         sub s1, s1, s0
-target: 
-        add s1, s1, s0
+target: add s1, s1, s0
+        j Shift
         nop
 ## branch_ex0.s ##
