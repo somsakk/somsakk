a42cc4e4fafb3673875366be7aa193485b2e1708
somsakk <88964214+somsakk@users.noreply.github.com>
2024-10-19 16:25:12 +0700

    Add files via upload
    Revise ports for UDPClient.py and UDPServer.py from 12000 to 22000 to avoid Wireshark thinking that the UDP packets use LLC protocol since ports 12000 - 12004 are for LLC.

--- web/python/UDPClient.py
+++ web/python/UDPClient.py
@@ -3,18 +3,18 @@
 
 # To call this code either type 'python3 UDPClient.py'
 # or put server's ip and port no. like
-# 'python3 UDPClient.py 127.0.0.1 12000'
-# or 'python3 UDPClient.py localhost 12000'
+# 'python3 UDPClient.py 127.0.0.1 22000'
+# or 'python3 UDPClient.py localhost 22000'
 
-# this code runs under python3.3 only (not python2.7)
+# this code runs under python3 (not python2)
 
 from socket import *
 import sys
 
 ## Allow user to input server's ip and port no.
-if len(sys.argv) == 1:  # the server is localhost and at port 12000  
+if len(sys.argv) == 1:  # the server is localhost and at port 22000  
 	serverName = '127.0.0.1'
-	serverPort = 12000
+	serverPort = 22000
 elif len(sys.argv) == 3: # use other server's address and port
 	serverName = sys.argv[1]
 	serverPort = int(sys.argv[2])
@@ -27,13 +27,12 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 # get a message from the user
 message = input('Input lowercase sentence:')  
 
-# send the message to the udpserver
+# send the message to the udp server
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 
 # receive the modified message back from the server
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
-#~print('Received message from server ', serverAddress, ' is ', modifiedMessage.decode('UTF-8'))
 print('Received message from server ', serverAddress, ' is ', modifiedMessage.decode())
 
 # close the socket
--- web/python/UDPServer.py
+++ web/python/UDPServer.py
@@ -1,8 +1,8 @@
 ## UDPServer.py ##
 # To call this code either type 'python3 UDPServer.py'
-# or put server's port no. like 'python3 UDPServer.py 12002'
+# or put server's port no. like 'python3 UDPServer.py 22002'
 
-# this code runs under python3.3 only (not python2.7)
+# this code runs under python3 (not python2)
 '''This is UDP server, which waits for client message and convert the received
 msg to upper letters, and then send the result back to the client'''
 
@@ -11,7 +11,7 @@ import sys
 
 ## Allow user's option to input port no.
 if len(sys.argv) == 1:  # use the pre-specified port
-	serverPort = 12000
+	serverPort = 22000
 elif len(sys.argv) == 2: # use user's specified port for the server to listen
 	serverPort = int(sys.argv[1])
 
@@ -25,6 +25,6 @@ print('The server is ready to receive.')
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
     print('Received message from client ', clientAddress, ' is ', message.decode())
-    modifiedMessage = message.decode().upper()
+    modifiedMessage = message.decode().upper() # convert the message to uppercase
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
 
